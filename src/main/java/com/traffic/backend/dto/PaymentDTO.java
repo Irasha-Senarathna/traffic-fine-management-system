4ccc08d6a6a626f4af7a5366e33b11e15af3c4f2
niluminda <kvcniluminda@gmail.com>
2026-06-09 20:39:41 +0530

    branch cleared

--- src/main/java/com/traffic/backend/dto/PaymentDTO.java
+++
@@ -1,9 +0,0 @@
-package com.traffic.backend.dto;
-
-import lombok.Data;
-
-@Data
-public class PaymentDTO {
-    private Long fineId;
-    private Double amount;
-}
--- src/main/java/com/traffic/backend/model/Role.java
+++
@@ -1,7 +0,0 @@
-package com.traffic.backend.model;
-
-public enum Role {
-    USER,
-    OFFICER,
-    ADMIN
-}
